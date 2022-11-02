b66db19d40976453f0b05b0cbe2a89092eac3cb6
iAmMortos <taylor0lopez@gmail.com>
2022-11-02 03:54:28 -0600

    Sync. Testing Changes

--- src/test/java/unit/BibtexStringTest.java
+++ src/test/java/unit/BibtexStringTest.java
@@ -6,31 +6,38 @@ import org.junit.Test;
 
 public class BibtexStringTest
 {
+  private static final String ID = "id";
+  private static final String NAME = "name";
+  private static final String CONTENT = "content";
+  private static final String NEW_ID = "new id";
+  private static final String NEW_NAME = "new name";
+  private static final String NEW_CONTENT = "new content";
+
   @Test
   public void testBibtexStringConstructAndGetters()
   {
-    BibtexString bibtexString = new BibtexString("id", "name", "content");
-    Assert.assertEquals(bibtexString.getId(), "id");
-    Assert.assertEquals(bibtexString.getName(), "name");
-    Assert.assertEquals(bibtexString.getContent(), "content");
+    BibtexString bibtexString = new BibtexString(ID, NAME, CONTENT);
+    Assert.assertEquals(bibtexString.getId(), ID);
+    Assert.assertEquals(bibtexString.getName(), NAME);
+    Assert.assertEquals(bibtexString.getContent(), CONTENT);
   }
 
   @Test
   public void testBibtexStringSetters()
   {
-    BibtexString bibtexString = new BibtexString("id", "name", "content");
-    bibtexString.setId("new id");
-    bibtexString.setName("new name");
-    bibtexString.setContent("new content");
-    Assert.assertEquals(bibtexString.getId(), "new id");
-    Assert.assertEquals(bibtexString.getName(), "new name");
-    Assert.assertEquals(bibtexString.getContent(), "new content");
+    BibtexString bibtexString = new BibtexString(ID, NAME, CONTENT);
+    bibtexString.setId(NEW_ID);
+    bibtexString.setName(NEW_NAME);
+    bibtexString.setContent(NEW_CONTENT);
+    Assert.assertEquals(bibtexString.getId(), NEW_ID);
+    Assert.assertEquals(bibtexString.getName(), NEW_NAME);
+    Assert.assertEquals(bibtexString.getContent(), NEW_CONTENT);
   }
 
   @Test
   public void testBibtexStringClone()
   {
-    BibtexString bibtexString = new BibtexString("id", "name", "content");
+    BibtexString bibtexString = new BibtexString(ID, NAME, CONTENT);
     BibtexString bibtexClone = (BibtexString) bibtexString.clone();
     Assert.assertEquals(bibtexString.getId(), bibtexClone.getId());
     Assert.assertEquals(bibtexString.getName(), bibtexClone.getName());
