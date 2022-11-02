ed5c00a33081593762633c31916e87910bba955d
iAmMortos <taylor0lopez@gmail.com>
2022-11-02 04:00:30 -0600

    Sync. Testing Changes

--- src/test/java/unit/BibtexStringComparatorTest.java
+++ src/test/java/unit/BibtexStringComparatorTest.java
@@ -1,5 +1,13 @@
 package test.java.unit;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BibtexStringComparatorTest
 {
+  @Test
+  public void testBibtexStringComparator()
+  {
+    Assert.fail();
+  }
 }
